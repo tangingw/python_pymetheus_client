7366eefd02060a83150f398a4d342cb6548c0032
Mobieus Jay <tangingw.pyconmy@gmail.com>
2021-09-22 11:20:59 +0800

    Preliminary Test Result

--- run.py
+++ run.py
@@ -8,21 +8,12 @@ register_device = RegisterDeviceHandler()
 device_id = register_device.get_device_id()
 
 pymetheus_client = PyMetheusDevice()
-pymetheus_client.select_endpoint("/device")
-
-pymetheus_client.select_endpoint(f"/device/{device_id}")
-device_status = pymetheus_client.get_data(device_id)
-"""device_status should contain
-{
-    "device_name": {
-        "device_id":
-        "port_id":
-        "service_id":
-        "network_id":
-    }
-}
+pymetheus_client.select_endpoint("/register")
 
 """
+pymetheus_client.select_endpoint(f"/device/{device_id}")
+device_status = pymetheus_client.get_data(device_id)
+
 if not device_status:
     print(
         "Device Not Registered"
@@ -45,3 +36,9 @@ else:
     )
 
     print("Send Event Data")
+"""
+
+#pymetheus_client.select_endpoint("/device")
+pymetheus_client.post_data(
+    data=register_device.get_device_metadata()
+)
